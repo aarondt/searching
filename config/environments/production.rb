ba888a3cced16cc7a0b1abf49462e7b302099295
aadt91 <aaron.tal@hotmail.de>
2016-09-15 17:57:39 +0000

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   
- config.assets.compile = true
- config.serve_static_files = false
+# config.assets.compile = true
+# config.serve_static_files = false
 end
