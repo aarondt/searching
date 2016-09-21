830a7ef82c945e6c9aeed52754f5c75096c5982f
aadt91 <aaron.tal@hotmail.de>
2016-09-21 09:23:01 +0000

    adjustments in the search logic to include case-insensitive searches

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -2,7 +2,8 @@ class BottlesController < ApplicationController
     def index   
          @categories = Bottle.uniq.pluck(:category)
          
-        @flaschen = Bottle.where(["name LIKE ? AND category LIKE ?","%#{params[:search]}%","%#{params[:category]}%"]).order(price: :ASC).page(params[:page]).per(20)
+         
+        @flaschen = Bottle.where(["lower(name) LIKE lower(?) AND category LIKE ?","%#{params[:search]}%","%#{params[:category]}%"]).order(price: :ASC).page(params[:page]).per(20)
        
         params[:quantity] = 1   if params[:quantity].blank?
         @final_price = Bottle.calculate_price(@flaschen, params[:quantity])        
